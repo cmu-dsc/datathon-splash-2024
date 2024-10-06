0051ec77df07cd848cfd3683313ee03e53785b3c
clementou <clement.h.ou@gmail.com>
2024-10-06 18:21:41 -0400

    fix?

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  assetPrefix: 'https://datathon-splash.netlify.app',
+  basePath: '/datathon',
   images: {
     domains: ['datathon-splash.netlify.app'],
+    unoptimized: true,
   },
 }
 
