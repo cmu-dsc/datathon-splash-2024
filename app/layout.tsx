ac7d20c8ebf8b37c0e292598f48ee9f5b796854c
clementou <clement.h.ou@gmail.com>
2024-10-30 16:51:10 -0400

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'  // This should work for both direct and proxy access
+import Script from 'next/script'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'], display: 'swap' })
 
@@ -9,13 +10,20 @@ export const metadata: Metadata = {
   description: 'Explore the environmental impact of AI in this CMU Data Science Club event',
 }
 
-export default function RootLayout({
+export default function RootLayout ({
   children,
 }: {
   children: React.ReactNode
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://analytics.umami.is/script.js"
+          data-website-id="7899076f-1eca-4c1e-bb90-db959ce41077"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
